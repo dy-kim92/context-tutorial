83aa28cd2799575a83dea39bf9f7da6b9d0f8ac5
dongyeon.kim <dongyeon.kim@lasee.io>
2023-04-07 10:49:19 +0900

    useContext Hook

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,28 +1,27 @@
-import { ColorConsumer } from "../contexts/color";
+import { useContext } from "react";
+import ColorContext from "../contexts/color";
 
 const ColorBox = () => {
-    return (
-        <ColorConsumer>
-            {(value) => (
-                <>
-                    <div
-                        style={{
-                            width: "64px",
-                            height: "64px",
-                            background: value.state.color,
-                        }}
-                    />
+    const { state } = useContext(ColorContext);
 
-                    <div
-                        style={{
-                            width: "32px",
-                            height: "32px",
-                            background: value.state.subcolor,
-                        }}
-                    />
-                </>
-            )}
-        </ColorConsumer>
+    return (
+        <>
+            <div
+                style={{
+                    width: "64px",
+                    height: "64px",
+                    background: state.color,
+                }}
+            />
+
+            <div
+                style={{
+                    width: "32px",
+                    height: "32px",
+                    background: state.subcolor,
+                }}
+            />
+        </>
     );
 };
 
